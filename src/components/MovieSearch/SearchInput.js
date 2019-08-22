2c0676bb690ffc8bd9052543fb67a25b30c89eab
kkch3n <kevin.kang.chen@gmail.com>
2019-08-22 15:18:30 -0700

    Removes console

--- src/components/MovieSearch/SearchInput.js
+++ src/components/MovieSearch/SearchInput.js
@@ -27,7 +27,6 @@ class SearchInput extends PureComponent {
 
     handleSelectChange = e => {
         let value = e.target.value
-        console.log(this.props)
         this.props.sortMovies(value)
     }
 
