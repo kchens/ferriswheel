1f68a84e19132b0f24a95f0d359399eab90e8675
kkch3n <kevin.kang.chen@gmail.com>
2019-08-22 13:59:00 -0700

    Adds movie styling

--- src/components/MovieSearch/Movie.js
+++ src/components/MovieSearch/Movie.js
@@ -3,20 +3,30 @@ import React from "react";
 const listItemStyle = {
     display: "flex",
     alignItems: "center",
-    margin: "1rem"
+    margin: "1rem",
 };
+
+const rightDetail = {
+    backgroundColor: "black",
+    opacity: "0.7",
+    width: "300px",
+    height: "450px",
+    color: "white",
+}
 
 const Movie = ({title, overview, release_date, poster_path }) => (
     <div style={listItemStyle}>
-        <h3>{title}</h3>
-        <div>{release_date}</div>
-        <div>{overview}</div>
-        <img 
-            srcSet={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path} 1x, https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path} 2x`} 
-            sizes="auto" 
+        <img
+            srcSet={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path} 1x, https://image.tmdb.org/t/p/w600_and_h900_bestv2${poster_path} 2x`}
+            sizes="auto"
             src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${poster_path}`}
             alt={title}
         />
+        <div style={rightDetail}>
+            <h2>{title}</h2>
+            <div style={{fontSize: "12px", marginBottom: "0.5rem"}}>{release_date}</div>
+            <div style={{margin: "1rem", textAlign: "justify"}}>{overview}</div>
+        </div>
     </div>
 );
 
