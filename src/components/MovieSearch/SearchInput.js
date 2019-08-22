c3c8dd4473db94b0cfaf0ce61d290c3f4377861d
kkch3n <kevin.kang.chen@gmail.com>
2019-08-22 13:20:18 -0700

    Changes to PureComponent; adds better UX for empty search value

--- src/components/MovieSearch/SearchInput.js
+++ src/components/MovieSearch/SearchInput.js
@@ -1,19 +1,23 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 
 const formStyle = {
     marginTop: "1rem"
 };
 
-class SearchInput extends Component {
+class SearchInput extends PureComponent {
     state = {
         value: ""
     };
 
-    handleChange = e => this.setState({ value: e.target.value })
+    handleChange = e => {
+        let value = e.target.value
+        this.setState({ value: value })
+        if (!value) return this.handleSubmit()
+    }
 
     handleSubmit = e => {
-        e.preventDefault();
-        this.props.searchMovies(this.state.value);
+        if (e) e.preventDefault()
+        this.props.searchMovies(this.state.value)
     }
 
     render() {
